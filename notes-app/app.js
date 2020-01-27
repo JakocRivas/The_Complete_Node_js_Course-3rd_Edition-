be88168d2965244a99c9514e363f7621eca8a016
Jakoc Rivas <jrivas@intellisys.com.do>
2020-01-27 09:41:26 -0400

    challenge 4: argument parsing with yargs library

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,56 @@
-const notes = require('./notes.js');
 const chalk = require('chalk');
+const yargs = require('yargs')
+const notes = require('./notes.js');
 
-const note = notes();
-console.log(note);
-console.log(chalk.green.bold.inverse("Success!"));
-//
-// Challenge: Use the chalk library in your project
-//
-// 1. Install version 2.4.1 of chalk
-// 2. Load chalk into app.js
-// 3. Use it to print the string "Success!" to the console in green
-// 4. Test your work
-//
-// Bonus: Use docs to mess around with other styles. Make text bold and inverse.
+// Customize yargs version
+yargs.version('1.1.0')
+
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('removing a note!')
+    }
+})
+
+// Challenge: Add two new commands
+// 
+//  1. Setup command to support "list" command (print place holder message for now)
+//  2. Setup command to support "read" command (print placeholder message for now)
+//  3. Test your work by running both commands and ensure correct output
+// 
+// 
+
+// Create remove command
+yargs.command({
+    command: 'list',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('listing notes!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'read',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('reading a note!')
+    }
+})
+
+// add, remove, read, list
+// console.log(process.argv);
+console.log(yargs.argv)
+
